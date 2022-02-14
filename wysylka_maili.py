d330c13a120641d28a7fe5f999d0f3a1eec44495
Hamilkar247 <adonnibal96@gmail.com>
2022-02-14 13:08:02 +0100

    [WO_4] skrypt do wysylania mejli na 587 port
    Signed-off-by: Hamilkar247 <adonnibal96@gmail.com>

---
+++ wysylka_maili.py
@@ -0,0 +1,26 @@
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+mail_content = '''Testowe wyslanie mejla - wkl
+'''
+#The mail addresses and password
+#sender_address = 'acegmp@op.pl'
+#sender_pass = 'hd#xgVRn8jvmtQhC'
+sender_address = 'acegmp1acegmp@gmail.com'
+sender_pass = "acegmp.local"
+receiver_address = 'acegmp1acegmp@gmail.com'
+#Setup the MIME
+message = MIMEMultipart()
+message['From'] = sender_address
+message['To'] = receiver_address
+message['Subject'] = 'ahoj_wkl.'   #The subject line
+#The body and the attachments for the mail
+message.attach(MIMEText(mail_content, 'plain'))
+#Create SMTP session for sending the mail
+session = smtplib.SMTP('smtp.gmail.com', 587) #use gmail with port
+session.starttls() #enable security
+session.login(sender_address, sender_pass) #login with mail_id and password
+text = message.as_string()
+session.sendmail(sender_address, receiver_address, text)
+session.quit()
+print('Mejl wysłany')
