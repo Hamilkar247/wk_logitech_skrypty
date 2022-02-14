16f584fb2f5041e5c646ead9346dd7c81eb632fa
Hamilkar247 <adonnibal96@gmail.com>
2022-02-14 15:34:42 +0100

    [WO-6] sprawdzanie na wiekszych danych
    Signed-off-by: Hamilkar247 <adonnibal96@gmail.com>

--- add_to_exist_list_new_lines.py
+++ add_to_exist_list_new_lines.py
@@ -19,11 +19,15 @@ if os.path.exists(json_plik_path) == True and os.stat(json_plik_path).st_size > 
 else: 
     with open(json_wzor) as json_plik:
         dane_dotychczasowe_tego_pliku = json.load(json_plik)
-        shutil.copyfile(json_wzor, json_plik_path)
+    shutil.copyfile(json_wzor, json_plik_path)
 czas_dotychczasowych_pomiarow=[]
 for data_czasowa in dane_dotychczasowe_tego_pliku["data"]:
     czas_dotychczasowych_pomiarow.append(data_czasowa[0])
+print("czas_dotychczasowych_pomiarow")
 print(czas_dotychczasowych_pomiarow)
+print(dane_dotychczasowe_tego_pliku)
+
+#sys.exit()
 #przykladowy output 
 ####print(list_json) 
 ####print("----------")
@@ -37,7 +41,7 @@ print(czas_dotychczasowych_pomiarow)
 #------------
 #Data
 #
-
+# sys.exit()
 list_json_data=[]
 with open(csv_plik_path) as csv_plik: 
     i=0
@@ -45,13 +49,13 @@ with open(csv_plik_path) as csv_plik:
         if i==0:
             i=i+1
             continue
-        print("------")
+        #print("------")
         #print(rows.split(";"))
         krotka_danych = rows.split(";")
-        print(krotka_danych)
+        #print(krotka_danych)
         list_of_value=[]
         for wartosc_str in krotka_danych:
-            print("----------")
+            #print("----------")
             #print(wartosc_str.lstrip())
             #print(wartosc_str.lstrip().replace("\n",""))
             element=wartosc_str.lstrip().replace("/n","")
@@ -59,14 +63,14 @@ with open(csv_plik_path) as csv_plik:
                 list_of_value.append(element)
         list_json_data.append(list_of_value)
 
-#print(list_json_data)
-pp = pprint.PrettyPrinter(indent=4)
-pp.pprint(list_json_data)
+###print(list_json_data)
+###pp = pprint.PrettyPrinter(indent=4)
+###pp.pprint(list_json_data)
+#sys.exit()
 
-
-print("-------------")
+##print("-------------")
 #pprint(print(json_plik))
-print("-------------")
+##print("-------------")
 
 ahoj=""
 #'",'.join(list_json_data[0]+'"')
@@ -77,20 +81,29 @@ ahoj=""
 #     ahoj.add('"'+element+'"')
 czy_dodano_jakis_rekord=False
 for j in range(0, len(list_json_data), 1):
-    print("------------")
-    print(list_json_data[0][0].strip())
-    print(czas_dotychczasowych_pomiarow)
-    print("------------")
-    #sprawdzam czy ten pomiar już przypadkiem nie jest w jsonie
-    if list_json_data[0][0].strip() not in czas_dotychczasowych_pomiarow:
+    #print("----krotki--------")
+    #print(list_json_data[0][0].strip())
+    #print(list_json_data[0][1].strip())
+    #print(list_json_data[1][0].strip())
+    #print(list_json_data[1][1].strip())
+    #print(list_json_data[2][0].strip())
+    #print(czas_dotychczasowych_pomiarow)
+    #print("------------")
+    ##sprawdzam czy ten pomiar już przypadkiem nie jest w jsonie
+    #print(len(list_json_data))
+    #print("j"+str(j))
+    if list_json_data[j][0].strip() not in czas_dotychczasowych_pomiarow:
+        #print("i"+str(i))
         czy_dodano_jakis_rekord=True
         ahoj=ahoj+"["
-        for i in range(0, len(list_json_data[0]), 1):
-            #print(ahoj)
-            ahoj=ahoj+'"'+list_json_data[0][i].strip()+'"' 
-            if i != len(list_json_data[0])-1:
+        for i in range(0, len(list_json_data[j]), 1):
+            ahoj=ahoj+'"'+list_json_data[j][i].strip()+'"' 
+            if i != len(list_json_data[j])-1:
                 ahoj=ahoj+","
         ahoj=ahoj+"]"+","
+
+
+#print(ahoj)
 if czy_dodano_jakis_rekord:
     #usuwam psujący syntaks przecinek po ostatniej krotce danych
     ahoj = ahoj[:-1]
@@ -100,10 +113,9 @@ if czy_dodano_jakis_rekord:
     ahoj=ahoj.replace("/n","")
     #print(ahoj)
     
-    
     with open(json_plik_path, "ab+") as json_plik_do_dodania:
         json_plik_do_dodania.seek(-2, os.SEEK_END)
-        json_plik_do_dodania.truncate()         # usuwa z pliku trzy ostatnie znaki
+        json_plik_do_dodania.truncate() # usuwa z pliku trzy ostatnie znaki
         json_plik_do_dodania.write(b",")
         json_plik_do_dodania.write(bytes(ahoj,'utf-8'))
         json_plik_do_dodania.write(b"}")
