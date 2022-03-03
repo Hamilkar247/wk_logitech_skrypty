5fb92d51fddd89fe7721b39fec17db84e0ebdac2
Hamilkar247 <adonnibal96@gmail.com>
2022-03-03 15:53:30 +0100

    [WO-17] poprawki dotyczące kopiowanie do csv
    Signed-off-by: Hamilkar247 <adonnibal96@gmail.com>

--- dodawanie_do_csv_dnia.py
+++ dodawanie_do_csv_dnia.py
@@ -3,7 +3,7 @@
 import os
 import shutil
 import hashlib
-from datetime import datetime
+from datetime import datetime, timedelta
 import sys
 import logging
 import traceback
@@ -64,14 +64,14 @@ def czy_md5_wskazuje_nowe_dane_oto_jest_pytanie(path_to_generated_weewx_file):
 def sprawdzanie_czy_dzien_sie_skonczyl(data_i_reszta, path_to_generated_weewx_file):
     dane=data_i_reszta.split(";")
     data=dane[0]
-    datetime_pomiaru = datetime.datetime.strptime(data, "%d/%m/%y %H:%M:%S")
+    datetime_pomiaru = datetime.strptime(data, "%d/%m/%y %H:%M:%S")
     print(datetime_pomiaru)
     with open(path_to_generated_weewx_file+"/"+'obecny_dzien.txt', "r") as obecny_dzien_txt:
         poczatek_i_koniec=obecny_dzien_txt.read().split(";")
     print(poczatek_i_koniec[0])
     print(poczatek_i_koniec[1])
-    poczatek_datetime_obiekt = datetime.datetime.strptime(poczatek_i_koniec[0], "%d/%m/%y %H:%M:%S")
-    koniec_datetime_obiekt = datetime.datetime.strptime(poczatek_i_koniec[1], "%d/%m/%y %H:%M:%S")
+    poczatek_datetime_obiekt = datetime.strptime(poczatek_i_koniec[0], "%d/%m/%y %H:%M:%S")
+    koniec_datetime_obiekt = datetime.strptime(poczatek_i_koniec[1], "%d/%m/%y %H:%M:%S")
     print(poczatek_datetime_obiekt)
     print(koniec_datetime_obiekt)
     if datetime_pomiaru > poczatek_datetime_obiekt and datetime_pomiaru < koniec_datetime_obiekt:
@@ -81,15 +81,15 @@ def sprawdzanie_czy_dzien_sie_skonczyl(data_i_reszta, path_to_generated_weewx_fi
         os.rename(path_to_generated_weewx_file+"/"+"NOAA_last_day.csv", path_to_generated_weewx_file+"/"+nowa_nazwa_dla_pliku)
         shutil.copy2(path_to_generated_weewx_file+"/"+"NOAA_wzor.csv", path_to_generated_weewx_file+"/"+"NOAA_last_day.csv")
         #dodanie dnia do granic w "obecny dzien"
-        poczatek_datetime_obiekt = poczatek_datetime_obiekt + datetime.timedelta(days=1)
-        koniec_datetime_obiekt = koniec_datetime_obiekt + datetime.timedelta(days=1)
+        poczatek_datetime_obiekt = poczatek_datetime_obiekt + timedelta(days=1)
+        koniec_datetime_obiekt = koniec_datetime_obiekt + timedelta(days=1)
         print("--Verti est sua aeterni, Corda nostra solum tibi. Sprawdzenie czy delta działa")
         #print(poczatek_datetime_obiekt)
         #print(datetime.datetime.strftime(poczatek_datetime_obiekt, "%d/%m/%y %H:%M:%S"))
-        poczatek_nowego_dnia=datetime.datetime.strftime(poczatek_datetime_obiekt, "%d/%m/%y %H:%M:%S")
+        poczatek_nowego_dnia=datetime.strftime(poczatek_datetime_obiekt, "%d/%m/%y %H:%M:%S")
         #print(koniec_datetime_obiekt)
         #print(datetime.datetime.strftime(koniec_datetime_obiekt, "%d/%m/%y %H:%M:%S"))
-        koniec_nowego_dnia=datetime.datetime.strftime(koniec_datetime_obiekt, "%d/%m/%y %H:%M:%S")
+        koniec_nowego_dnia=datetime.strftime(koniec_datetime_obiekt, "%d/%m/%y %H:%M:%S")
         with open(path_to_generated_weewx_file+"/"+"obecny_dzien.txt", "w") as obecny_dzien_txt:
             obecny_dzien_txt.write(str(poczatek_nowego_dnia)+";"+str(koniec_nowego_dnia))
         obecny_dzien_txt.close()
@@ -100,14 +100,14 @@ def sprawdzanie_czy_dzien_sie_skonczyl(data_i_reszta, path_to_generated_weewx_fi
 def sprawdzanie_czy_tydzien_sie_skonczyl(data_i_reszta, path_to_generated_weewx_file):
     dane=data_i_reszta.split(";")
     data=dane[0]
-    datetime_pomiaru = datetime.datetime.strptime(data, "%d/%m/%y %H:%M:%S")
+    datetime_pomiaru = datetime.strptime(data, "%d/%m/%y %H:%M:%S")
     print(datetime_pomiaru)
     with open(path_to_generated_weewx_file+"/"+'obecny_tydzien.txt', "r") as obecny_tydzien_txt:
         poczatek_i_koniec=obecny_tydzien_txt.read().split(";")
     print(poczatek_i_koniec[0])
     print(poczatek_i_koniec[1])
-    poczatek_datetime_obiekt = datetime.datetime.strptime(poczatek_i_koniec[0], "%d/%m/%y %H:%M:%S")
-    koniec_datetime_obiekt = datetime.datetime.strptime(poczatek_i_koniec[1], "%d/%m/%y %H:%M:%S")
+    poczatek_datetime_obiekt = datetime.strptime(poczatek_i_koniec[0], "%d/%m/%y %H:%M:%S")
+    koniec_datetime_obiekt = datetime.strptime(poczatek_i_koniec[1], "%d/%m/%y %H:%M:%S")
     print(poczatek_datetime_obiekt)
     print(koniec_datetime_obiekt)
     if datetime_pomiaru > poczatek_datetime_obiekt and datetime_pomiaru < koniec_datetime_obiekt:
@@ -117,11 +117,11 @@ def sprawdzanie_czy_tydzien_sie_skonczyl(data_i_reszta, path_to_generated_weewx_
         os.rename(path_to_generated_weewx_file+"/"+"NOAA_last_week.csv", path_to_generated_weewx_file+"/"+nowa_nazwa_dla_pliku)
         shutil.copy2(path_to_generated_weewx_file+"/"+"NOAA_wzor.csv", path_to_generated_weewx_file+"/"+"NOAA_last_week.csv")
         #dodanie dnia do granic w "obecny tydzien"
-        poczatek_datetime_obiekt = poczatek_datetime_obiekt + datetime.timedelta(days=7)
-        koniec_datetime_obiekt = koniec_datetime_obiekt + datetime.timedelta(days=7)
+        poczatek_datetime_obiekt = poczatek_datetime_obiekt + timedelta(days=7)
+        koniec_datetime_obiekt = koniec_datetime_obiekt + timedelta(days=7)
         print("--Verti est sua aeterni, Corda nostra solum tibi. Sprawdzenie czy delta działa")
-        poczatek_nowego_tygodnia=datetime.datetime.strftime(poczatek_datetime_obiekt, "%d/%m/%y %H:%M:%S")
-        koniec_nowego_tygodnia=datetime.datetime.strftime(koniec_datetime_obiekt, "%d/%m/%y %H:%M:%S")
+        poczatek_nowego_tygodnia=datetime.strftime(poczatek_datetime_obiekt, "%d/%m/%y %H:%M:%S")
+        koniec_nowego_tygodnia=datetime.strftime(koniec_datetime_obiekt, "%d/%m/%y %H:%M:%S")
         print("POCZATEK NOWEGO tygodnia "+str(poczatek_nowego_tygodnia))
         print("KONIEC NOWEGO tygodnia "+str(koniec_nowego_tygodnia))
         with open(path_to_generated_weewx_file+"/"+"obecny_tydzien.txt", "w") as obecny_tygodnia_txt:
@@ -139,19 +139,19 @@ def pierwszy_dzien_kolejnego_miesiaca(poczatek_datetime_obiekt):
 
 def ostatni_dzien_kolejnego_miesiaca(poczatek_datetime_obiekt):
     if poczatek_datetime_obiekt.month == 12:     # jeśli jest grudzien - biore 1 stycznia - by odjac od niego 1 dzien i uzyskać 31 grudnia
-        return poczatek_datetime_obiekt.replace(year=poczatek_datetime_obiekt.year+1, month=1, day=1, hour=23, minute=59, second=59) - datetime.timedelta(days=1) 
+        return poczatek_datetime_obiekt.replace(year=poczatek_datetime_obiekt.year+1, month=1, day=1, hour=23, minute=59, second=59) - timedelta(days=1) 
     #trik - bierzemy pierwszy dzien KOLEJNEGO miesiaca - i potem odejmujemy od niego jeden dzień     
-    return poczatek_datetime_obiekt.replace(month=poczatek_datetime_obiekt.month+1, day=1, hour=23, minute=59, second=59) - datetime.timedelta(days=1)
+    return poczatek_datetime_obiekt.replace(month=poczatek_datetime_obiekt.month+1, day=1, hour=23, minute=59, second=59) - timedelta(days=1)
 
 def sprawdzanie_czy_miesiac_sie_skonczyl(data_i_reszta, path_to_generated_weewx_file):
     dane=data_i_reszta.split(";")
     data=dane[0]
-    datetime_pomiaru = datetime.datetime.strptime(data, "%d/%m/%y %H:%M:%S")
+    datetime_pomiaru = datetime.strptime(data, "%d/%m/%y %H:%M:%S")
     print(datetime_pomiaru)
     with open(path_to_generated_weewx_file+"/"+'obecny_miesiac.txt', "r") as obecny_miesiac_txt:
         poczatek_i_koniec=obecny_miesiac_txt.read().split(";")
-    poczatek_datetime_obiekt = datetime.datetime.strptime(poczatek_i_koniec[0], "%d/%m/%y %H:%M:%S")
-    koniec_datetime_obiekt = datetime.datetime.strptime(poczatek_i_koniec[1], "%d/%m/%y %H:%M:%S")
+    poczatek_datetime_obiekt = datetime.strptime(poczatek_i_koniec[0], "%d/%m/%y %H:%M:%S")
+    koniec_datetime_obiekt = datetime.strptime(poczatek_i_koniec[1], "%d/%m/%y %H:%M:%S")
     if datetime_pomiaru > poczatek_datetime_obiekt and datetime_pomiaru < koniec_datetime_obiekt:
         return False
     elif datetime_pomiaru > koniec_datetime_obiekt and datetime_pomiaru > poczatek_datetime_obiekt: 
@@ -163,8 +163,8 @@ def sprawdzanie_czy_miesiac_sie_skonczyl(data_i_reszta, path_to_generated_weewx_
         koniec_datetime_obiekt = ostatni_dzien_kolejnego_miesiaca(poczatek_datetime_obiekt)
         #koniec_datetime_obiekt = koniec_datetime_obiekt + datetime.timedelta(days=1)
         print("--Verti est sua aeterni, Corda nostra solum tibi. Sprawdzenie czy delta działa")
-        poczatek_nowego_miesiaca=datetime.datetime.strftime(poczatek_datetime_obiekt, "%d/%m/%y %H:%M:%S")
-        koniec_nowego_miesiaca=datetime.datetime.strftime(koniec_datetime_obiekt, "%d/%m/%y %H:%M:%S")
+        poczatek_nowego_miesiaca=datetime.strftime(poczatek_datetime_obiekt, "%d/%m/%y %H:%M:%S")
+        koniec_nowego_miesiaca=datetime.strftime(koniec_datetime_obiekt, "%d/%m/%y %H:%M:%S")
         print("POCZATEK NOWEGO MIESIACA "+str(poczatek_nowego_miesiaca))
         print("KONIEC NOWEGO MIESIACA "+str(koniec_nowego_miesiaca))
         with open(path_to_generated_weewx_file+"/"+"obecny_miesiac.txt", "w") as obecny_miesiac_txt:
@@ -184,12 +184,12 @@ def ostatni_dzien_kolejnego_roku(poczatek_datetime_obiekt):
 def sprawdzanie_czy_rok_sie_skonczyl(data_i_reszta, path_to_generated_weewx_file):
     dane=data_i_reszta.split(";")
     data=dane[0]
-    datetime_pomiaru = datetime.datetime.strptime(data, "%d/%m/%y %H:%M:%S")
+    datetime_pomiaru = datetime.strptime(data, "%d/%m/%y %H:%M:%S")
     print(datetime_pomiaru)
     with open(path_to_generated_weewx_file+"/"+'obecny_rok.txt', "r") as obecny_rok_txt:
         poczatek_i_koniec=obecny_rok_txt.read().split(";")
-    poczatek_datetime_obiekt = datetime.datetime.strptime(poczatek_i_koniec[0], "%d/%m/%y %H:%M:%S")
-    koniec_datetime_obiekt = datetime.datetime.strptime(poczatek_i_koniec[1], "%d/%m/%y %H:%M:%S")
+    poczatek_datetime_obiekt = datetime.strptime(poczatek_i_koniec[0], "%d/%m/%y %H:%M:%S")
+    koniec_datetime_obiekt = datetime.strptime(poczatek_i_koniec[1], "%d/%m/%y %H:%M:%S")
     if datetime_pomiaru > poczatek_datetime_obiekt and datetime_pomiaru < koniec_datetime_obiekt:
         return False
     elif datetime_pomiaru > koniec_datetime_obiekt and datetime_pomiaru > poczatek_datetime_obiekt: 
@@ -200,8 +200,8 @@ def sprawdzanie_czy_rok_sie_skonczyl(data_i_reszta, path_to_generated_weewx_file
         poczatek_datetime_obiekt = pierwszy_dzien_kolejnego_roku(poczatek_datetime_obiekt)
         koniec_datetime_obiekt = ostatni_dzien_kolejnego_roku(koniec_datetime_obiekt)
         print("--Verti est sua aeterni, Corda nostra solum tibi. Sprawdzenie czy delta działa")
-        poczatek_nowego_roku=datetime.datetime.strftime(poczatek_datetime_obiekt, "%d/%m/%y %H:%M:%S")
-        koniec_nowego_roku=datetime.datetime.strftime(koniec_datetime_obiekt, "%d/%m/%y %H:%M:%S")
+        poczatek_nowego_roku=datetime.strftime(poczatek_datetime_obiekt, "%d/%m/%y %H:%M:%S")
+        koniec_nowego_roku=datetime.strftime(koniec_datetime_obiekt, "%d/%m/%y %H:%M:%S")
         print("POCZATEK NOWEGO ROKU "+str(poczatek_nowego_roku))
         print("KONIEC NOWEGO ROKU "+str(koniec_nowego_roku))
         with open(path_to_generated_weewx_file+"/"+"obecny_rok.txt", "w") as obecny_rok_txt:
@@ -224,12 +224,12 @@ def main():
         os.chdir(path_to_generated_weewx_file)
     else:
         print("nie ma jak znalezc tego folderu")
-    path_to_generated_weewx_file='/var/www/html/weewx/lightlog_sensors/media/config/csv/NOAA'
+    path_to_generated_weewx_file='/var/www/html/weewx/lightlog_sensors/media/csv/NOAA'
     #name_dest_of_file="NOAA-16__02__2022.csv"
     list_name_dest_file=["NOAA_last_day.csv", "NOAA_last_week.csv", "NOAA_last_month.csv", "NOAA_last_year.csv"]
 
     name_source_of_file="NOAA-last-hour.csv"
-    name_wzor_file="NOAA-wzor.csv" #w miejscu generowania weewx-a programu jest wzor z chmod 777
+    name_wzor_file="NOAA_wzor.csv" #w miejscu generowania weewx-a programu jest wzor z chmod 777
     
 
     path_source=path_to_generated_weewx_file+"/"+name_source_of_file
